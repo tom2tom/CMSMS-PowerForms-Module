475845be2628543761487cb3396a805f6395800c
tom2tom <tpgww@onepost.net>
2015-05-13 18:49:14 +1000

    refactor defaultadmin & related

--- action.defaultadmin.php
+++ action.defaultadmin.php
@@ -1,137 +1,49 @@
 <?php
 # This file is part of CMS Made Simple module: PowerForms
 # Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
 # Refer to licence and other details at the top of file PowerForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
 
-//list all the extant forms
-$forms = $this->GetForms();
-$num_forms = count($forms);
+$padm = $this->CheckPermission('ModifyPFSettings');
+$pmod = $this->CheckPermission('ModifyPFForms');
+if(!($padm || $pmod)) exit;
+$pdev = $this->CheckPermission('Modify Any Page');
 
-$pmod = $this->CheckPermission('ModifyForms');
-
-$smarty->assign('tabheaders', $this->StartTabHeaders() .
-	$this->SetTabHeader('forms',$this->Lang('forms')) .
-	$this->SetTabHeader('config',$this->Lang('configuration')) .
-	$this->EndTabHeaders().
-	$this->StartTabContent());
-$smarty->assign('start_formtab',$this->StartTab("forms"));
-$smarty->assign('start_configtab',$this->StartTab("config"));
-$smarty->assign('end_tab',$this->EndTab());
-$smarty->assign('end_tabs',$this->EndTabContent());
-$smarty->assign('title_form_name',$this->Lang('title_form_name'));
-$smarty->assign('title_page_tag',$this->Lang('title_page_tag'));
-$smarty->assign('start_configform',$this->CreateFormStart($id,'update_config',$returnid));
-$smarty->assign('message', isset($params['pwfp_message'])?$params['pwfp_message']:'');
-
-if($pmod)
+if($padm)
 {
-	$theme = $gCms->variables['admintheme'];
-	$iconexport = '<img src="'.$this->GetModuleURLPath().'/images/xml.gif" class="systemicon" title="'.$this->Lang('export').'" alt="'.$this->Lang('export_tip').'" />';
-	$iconedit = $theme->DisplayImage('icons/system/edit.gif',$this->Lang('edit'),'','','systemicon');
-	$iconcopy = $theme->DisplayImage('icons/system/copy.gif',$this->Lang('copy'),'','','systemicon');
-	$icondelete = $theme->DisplayImage('icons/system/delete.gif',$this->Lang('delete'),'','','systemicon');
-}
-
-$formArray = array();
-foreach($forms as $thisForm)
-{
-	$oneset = new stdClass();
-	if($pmod)
+	if(isset($params['submit']))
 	{
-		$oneset->name = $this->CreateLink($id,'add_edit_form','',
-			$thisForm['name'],array('form_id'=>$thisForm['form_id']));
-		$oneset->xmllink = $this->CreateLink($id,'export_form','',
-			$iconexport,array('form_id'=>$thisForm['form_id']));
-		$oneset->editlink = $this->CreateLink($id,'add_edit_form','',
-			$iconedit,array('form_id'=>$thisForm['form_id']));
-		$oneset->copylink = $this->CreateLink($id,'copy_form','',
-			$iconcopy,array('form_id'=>$thisForm['form_id']));
-		$oneset->deletelink = $this->CreateLink($id,'delete_form','',
-			$icondelete,array('form_id'=>$thisForm['form_id']),
-			$this->Lang('are_you_sure_delete_form',$thisForm['name']));
+		$this->SetPreference('blank_invalid',!empty($params['blank_invalid']));
+		$this->SetPreference('enable_antispam',!empty($params['enable_antispam']));
+		$this->SetPreference('require_fieldnames',!empty($params['require_fieldnames']));
+		$t = trim($params['uploads_dir']);
+		if($t && $t[0] == DIRECTORY_SEPARATOR)
+			$t = substr($t,1);
+		if($t)
+		{
+			$fp = $config['uploads_path'];
+			if($fp && is_dir($fp))
+			{
+				$fp = $fp.DIRECTORY_SEPARATOR.$t;
+				if(!(is_dir($fp) || mkdir($fp,0644)))
+					$t = '';
+			}
+			else
+				$t = '';
+		}
+		$this->SetPreference('uploads_dir',$t);
+
+		$params['message'] = $this->PrettyMessage('settings_updated');
+		$params['active_tab'] = 'settings';
 	}
-	else
+	elseif(isset($params['cancel']))
 	{
-		$oneset->name = $thisForm['name'];
-		$oneset->xmllink = '';
-		$oneset->editlink = '';
-		$oneset->copylink = '';
-		$oneset->deletelink = '';
+		$params['active_tab'] = 'settings';
 	}
-	$oneset->usage = $thisForm['alias'];
-	$formArray[] = $oneset;
 }
 
-if($pmod)
-{
-	$smarty->assign('addlink',$this->CreateLink($id,'add_edit_form', '',
-		$theme->DisplayImage('icons/system/newobject.gif',$this->Lang('title_add_new_form'),'','','systemicon'),
-		array()));
-	$smarty->assign('addform',$this->CreateLink($id,'add_edit_form','',$this->Lang('title_add_new_form'),
-		array()));
-	$smarty->assign('may_config',1);
-}
-else
-	$smarty->assign('may_config',0);
+require dirname(__FILE__).DIRECTORY_SEPARATOR.'method.defaultadmin.php';
 
-$smarty->assign_by_ref('forms', $formArray);
-
-$smarty->assign('title_hide_errors',$this->Lang('title_hide_errors'));
-$smarty->assign('input_hide_errors',$this->CreateInputCheckbox($id, 'pwfp_hide_errors', 1, $this->GetPreference('hide_errors','0')). $this->Lang('title_hide_errors_long'));
-
-$smarty->assign('title_relaxed_email_regex',$this->Lang('title_relaxed_email_regex'));
-$smarty->assign('input_relaxed_email_regex',$this->CreateInputCheckbox($id, 'pwfp_relaxed_email_regex', 1, $this->GetPreference('relaxed_email_regex','0')). $this->Lang('title_relaxed_regex_long'));
-
-$smarty->assign('title_enable_fastadd',$this->Lang('title_enable_fastadd'));
-$smarty->assign('input_enable_fastadd',$this->CreateInputCheckbox($id, 'pwfp_enable_fastadd', 1, $this->GetPreference('enable_fastadd','1')). $this->Lang('title_enable_fastadd_long'));
-
-$smarty->assign('title_require_fieldnames',$this->Lang('title_require_fieldnames'));
-$smarty->assign('input_require_fieldnames',$this->CreateInputCheckbox($id, 'pwfp_require_fieldnames', 1, $this->GetPreference('require_fieldnames','1')). $this->Lang('title_require_fieldnames_long'));
-
-$smarty->assign('title_unique_fieldnames',$this->Lang('title_unique_fieldnames'));
-$smarty->assign('input_unique_fieldnames',$this->CreateInputCheckbox($id, 'pwfp_unique_fieldnames', 1, $this->GetPreference('unique_fieldnames','1')). $this->Lang('title_unique_fieldnames_long'));
-
-$smarty->assign('title_enable_antispam',$this->Lang('title_enable_antispam'));
-$smarty->assign('input_enable_antispam',$this->CreateInputCheckbox($id, 'pwfp_enable_antispam', 1, $this->GetPreference('enable_antispam','1')). $this->Lang('title_enable_antispam_long'));
-
-$smarty->assign('title_show_fieldids',$this->Lang('title_show_fieldids'));
-$smarty->assign('input_show_fieldids',
-	$this->CreateInputcheckbox($id,'pwfp_show_fieldids',1,
-	$this->GetPreference('show_fieldids','0')). $this->Lang('title_show_fieldids_long'));
-
-$smarty->assign('title_show_fieldaliases',$this->Lang('title_show_fieldaliases'));
-$smarty->assign('input_show_fieldaliases',
-	$this->CreateInputcheckbox($id,'pwfp_show_fieldaliases',1,
-	$this->GetPreference('show_fieldaliases','1')). $this->Lang('title_show_fieldaliases_long'));
-
-$smarty->assign('title_show_version',$this->Lang('title_show_version'));
-$smarty->assign('input_show_version',$this->CreateInputCheckbox($id, 'pwfp_show_version', 1, $this->GetPreference('show_version','1')). $this->Lang('title_show_version_long'));
-
-$smarty->assign('title_blank_invalid',$this->Lang('title_blank_invalid'));
-$smarty->assign('input_blank_invalid',$this->CreateInputCheckbox($id,
-  'pwfp_blank_invalid', 1, $this->GetPreference('blank_invalid','0')).
-  $this->Lang('title_blank_invalid_long'));
-
-$smarty->assign('submit', $this->CreateInputSubmit($id, 'pwfp_submit', $this->Lang('save')));
-$smarty->assign('end_configform',$this->CreateFormEnd());
-
-$smarty->assign('start_xmlform',$this->CreateFormStart($id, 'import_form', $returnid, 'POST','multipart/form-data'));
-$smarty->assign('submitxml', $this->CreateInputSubmit($id, 'pwfp_submit', $this->Lang('upload')));
-$smarty->assign('end_xmlform',$this->CreateFormEnd());
-
-$smarty->assign('input_xml_to_upload',$this->CreateInputFile($id, 'pwfp_xmlfile', '', 25));
-$smarty->assign('title_xml_to_upload',$this->Lang('title_xml_to_upload'));
-$smarty->assign('title_xml_upload_formname',$this->Lang('title_xml_upload_formname'));
-$smarty->assign('input_xml_upload_formname',
-	  $this->CreateInputText($id,'pwfp_import_formname','',25));
-$smarty->assign('title_xml_upload_formalias',$this->Lang('title_xml_upload_formalias'));
-$smarty->assign('input_xml_upload_formalias',
-	  $this->CreateInputText($id,'pwfp_import_formalias','',25));
-$smarty->assign('info_leaveempty',$this->Lang('help_leaveempty'));
-$smarty->assign('legend_xml_import',$this->Lang('title_import_legend'));
-
-echo $this->ProcessTemplate('AdminMain.tpl');
+echo $this->ProcessTemplate('adminpanel.tpl');
 
 ?>
---
+++ method.defaultadmin.php
@@ -0,0 +1,211 @@
+<?php
+# This file is part of CMS Made Simple module: PowerForms
+# Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
+# Refer to licence and other details at the top of file PowerForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+
+$tab = $this->GetActiveTab($params);
+
+$starts = $this->StartTabHeaders().
+	$this->SetTabHeader('maintab',$this->Lang('forms'),($tab == 'maintab'));
+if($pmod)
+	$starts .= $this->SetTabHeader('import',$this->Lang('import'),($tab == 'import'));
+if($padm)
+	$starts .= $this->SetTabHeader('settings',$this->Lang('settings'),($tab == 'settings'));
+$starts .= $this->EndTabHeaders().$this->StartTabContent();
+
+$smarty->assign('tabs_start',$starts);
+$smarty->assign('formstab_start',$this->StartTab('maintab'));
+if($pmod)
+	$smarty->assign('importstab_start',$this->StartTab('import'));
+if($padm)
+	$smarty->assign('settingstab_start',$this->StartTab('settings'));
+$smarty->assign('tab_end',$this->EndTab());
+$smarty->assign('tabs_end',$this->EndTabContent());
+$smarty->assign('form_end',$this->CreateFormEnd());
+
+$smarty->assign('message',(isset($params['message']))?$params['message']:'');
+
+$theme = $gCms->variables['admintheme'];
+
+//list all the extant forms
+$allforms = pwfUtils::GetForms();
+if($allforms)
+{
+	$smarty->assign('title_name',$this->Lang('title_form_name'));
+	if($pdev)
+		$smarty->assign('title_alias',$this->Lang('title_page_tag'));
+	else
+		$smarty->assign('title_alias',$this->Lang('title_alias'));
+
+	if($pmod)
+	{
+		$iconedit = $theme->DisplayImage('icons/system/edit.gif',$this->Lang('edit'),'','','systemicon');
+		$iconcopy = $theme->DisplayImage('icons/system/copy.gif',$this->Lang('copy'),'','','systemicon');
+		$icondelete = $theme->DisplayImage('icons/system/delete.gif',$this->Lang('delete'),'','','systemicon');
+	}
+	$iconexport = '<img src="'.$this->GetModuleURLPath().'/images/xml.gif" class="systemicon" title="'.$this->Lang('export').'" alt="'.$this->Lang('export_tip').'" />';
+	if($pdev)
+		$modname = $this->GetName();
+	$data = array();
+	foreach($allforms as $one)
+	{
+		$fid = (int)$one['form_id'];
+		$oneset = new stdClass();
+		if($pmod)
+		{
+			$oneset->name = $this->CreateLink($id,'add_edit_form','',
+				$one['name'],array('form_id'=>$fid));
+			$oneset->editlink = $this->CreateLink($id,'add_edit_form','',
+				$iconedit,array('form_id'=>$fid));
+			$oneset->copylink = $this->CreateLink($id,'copy_form','',
+				$iconcopy,array('form_id'=>$fid));
+			$oneset->deletelink = $this->CreateLink($id,'delete_form','',
+				$icondelete,array('form_id'=>$fid),
+				$this->Lang('confirm_delete_form',$one['name']));
+		}
+		else
+		{
+			$oneset->name = $one['name'];
+		}
+		$oneset->alias = ($pdev) ?
+			'{'.$modname.' form=\''.$one['alias'].'\'}' : $one['alias'];
+		$oneset->exportlink = $this->CreateLink($id,'export_form','',
+			$iconexport,array('form_id'=>$fid));
+		$oneset->selected = $this->CreateInputCheckbox($id,'sel[]',$fid,-1);
+		$data[] = $oneset;
+	}
+	$smarty->assign('forms',$data);
+
+	if(count($data) > 1)
+		$t = $this->CreateInputCheckbox($id,'item',true,false,'onclick="select_all(this);"');
+	else
+		$t = '';
+	$smarty->assign('selectall_forms',$t);
+	$smarty->assign('start_formsform',$this->CreateFormStart($id,'selected_forms',$returnid));
+	$smarty->assign('exportbtn',$this->CreateInputSubmit($id,'export',$this->Lang('export'),
+		'title="'.$this->Lang('tip_exportsel').'" onclick="return any_selected();"'));
+	if($pmod)
+	{
+		$smarty->assign('clonebtn',$this->CreateInputSubmit($id,'clone',$this->Lang('copy'),
+			'title="'.$this->Lang('tip_clonesel').'" onclick="return any_selected();"'));		
+		$smarty->assign('deletebtn',$this->CreateInputSubmit($id,'delete',$this->Lang('delete'),
+			'title="'.$this->Lang('tip_deletesel').'" onclick="return confirm_selected(\''.
+			$this->Lang('confirm').'\');"'));		
+	}
+}
+else
+{
+	$smarty->assign('noforms',$this->Lang('no_forms'));
+}
+
+if($pmod)
+{
+	$smarty->assign('addlink',$this->CreateLink($id,'add_edit_form', '',
+		$theme->DisplayImage('icons/system/newobject.gif',$this->Lang('title_add_new_form'),'','','systemicon')));
+	$smarty->assign('addform',$this->CreateLink($id,'add_edit_form','',
+		$this->Lang('title_add_new_form')));
+
+	$xmls = array();
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_xml_to_upload');
+	$oneset->input = $this->CreateInputFile($id,'xmlfile','',25);
+	$xmls[] = $oneset;
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_xml_upload_formname');
+	$oneset->input = $this->CreateInputText($id,'import_formname','',25);
+	$oneset->help = $this->Lang('help_import_name');
+	$xmls[] = $oneset;
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_xml_upload_formalias');
+	$oneset->input = $this->CreateInputText($id,'import_formalias','',25);
+	$oneset->help = $this->Lang('help_import_alias');
+	$xmls[] = $oneset;
+
+	$smarty->assign('legend_xmlimport',$this->Lang('title_importxml_legend'));
+	$smarty->assign('start_importxmlform',$this->CreateFormStart($id,'import_formfile',$returnid,'POST','multipart/form-data'));
+	$smarty->assign('xmls',$xmls);
+	$smarty->assign('submitxml', $this->CreateInputSubmit($id,'submitxml', $this->Lang('upload')));
+
+	$ob = $this->GetModuleInstance('FormBuilder');
+	if($ob)
+	{
+		unset($ob);
+		$smarty->assign('legend_fbimport',$this->Lang('title_importfb_legend'));
+		$smarty->assign('start_importfbform',$this->CreateFormStart($id,'import_formbrowser',$returnid));
+		$smarty->assign('submitfb',$this->CreateInputSubmit($id,'importforms',
+			$this->Lang('import_fb'),
+			'title="'.$this->Lang('tip_import_fb').'"'));
+		$smarty->assign('submitfbdata',$this->CreateInputSubmit($id,'importdata',
+			$this->Lang('import_fbdata'),
+			'title="'.$this->Lang('tip_import_fbdata').'"'));
+	}
+	$smarty->assign('pmod',1);
+}
+else
+	$smarty->assign('pmod',0);
+
+if($padm)
+{
+	$cfgs = array();
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_require_fieldnames');
+	$oneset->input = $this->CreateInputCheckbox($id,'require_fieldnames',1,$this->GetPreference('require_fieldnames'));
+	$oneset->help = $this->Lang('help_require_fieldnames');
+	$cfgs[] = $oneset;
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_blank_invalid');
+	$oneset->input = $this->CreateInputCheckbox($id,'blank_invalid',1,$this->GetPreference('blank_invalid'));
+	$oneset->help = $this->Lang('help_blank_invalid');
+	$cfgs[] = $oneset;
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_enable_antispam');
+	$oneset->input = $this->CreateInputCheckbox($id,'enable_antispam',1,$this->GetPreference('enable_antispam'));
+	$oneset->help = $this->Lang('help_enable_antispam');
+	$cfgs[] = $oneset;
+
+	$oneset = new stdClass();
+	$oneset->title = $this->Lang('title_uploads_dir');
+	$oneset->input = $this->CreateInputText($id,'uploads_dir',$this->GetPreference('uploads_dir'),40,80);
+	$oneset->help = $this->Lang('help_uploads_dir');
+	$cfgs[] = $oneset;
+
+	$smarty->assign('configs',$cfgs);
+	$smarty->assign('start_configform',$this->CreateFormStart($id,'defaultadmin',$returnid));
+	$smarty->assign('submitcfg', $this->CreateInputSubmit($id,'submit',$this->Lang('save')));
+	$smarty->assign('padm',1);
+}
+else
+	$smarty->assign('padm',0);
+
+$js = <<<EOS
+function select_all(b) {
+ var st = $(b).attr('checked');
+ if(!st) st = false;
+ $('input[name="{$id}sel[]"][type="checkbox"]').attr('checked',st);
+}
+function sel_count() {
+ var cb = $('input[name="{$id}sel[]"]:checked');
+ return cb.length;
+}
+function any_selected() {
+ return (sel_count() > 0);
+}
+function confirm_selected(msg) {
+ if(sel_count() > 0) {
+  return confirm(msg);
+ } else {
+  return false;
+ }
+}
+EOS;
+
+$smarty->assign('jsfuncs',$js);
+
+?>
