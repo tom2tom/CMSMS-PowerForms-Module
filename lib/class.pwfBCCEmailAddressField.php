3bc376521e920b80adc336d7c8c45a06c7d75c1b
tom2tom <tpgww@onepost.net>
2015-05-15 12:16:16 +1000

    new fields

---
+++ lib/class.pwfBCCEmailAddressField.php
@@ -0,0 +1,116 @@
+<?php
+# This file is part of CMS Made Simple module: PowerForms
+# Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
+# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Refer to licence and other details at the top of file PowerForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+//TODO 
+class pwfCCEmailAddressField extends pwfFieldBase
+{
+	function __construct(&$formdata, &$params)
+	{
+		parent::__construct($formdata, $params);
+		$this->IsInput = TRUE;
+		$this->Type = 'BCCEmailAddressField';
+		$this->ValidationType = 'email';
+		$this->ValidationTypes = array($formdata->pwfmodule->Lang('validation_email_address')=>'email');
+		$this->modifiesOtherFields = TRUE;
+	}
+
+	function GetFieldInput($id, &$params, $returnid)
+	{
+		$mod = $this->formdata->pwfmodule;
+		$js = $this->GetOption('javascript','');
+
+		return $mod->CustomCreateInputType($id, '_'.$this->Id,
+			htmlspecialchars($this->Value, ENT_QUOTES),
+           25,128,$js.$this->GetCSSIdTag());
+	}
+
+	function PrePopulateAdminForm($formDescriptor)
+	{
+		$mod = $this->formdata->pwfmodule;
+		$main = array();
+		$fieldlist = array();
+		$others = $this->formdata->Fields;
+		foreach($others as &$one)
+		{
+			if($one->IsDisposition()
+				&& is_subclass_of($one,'pwfDispositionEmailBase'))
+			{
+				$txt = $one->GetName().': '.$one->GetDisplayType();
+				$alias = $one->GetAlias();
+				if(!empty($alias))
+					$txt .= ' ('.$alias.')';
+
+				$fieldlist[$txt] = $one->GetId();
+			}
+		}
+		unset ($one);
+
+		$main[] = array($mod->Lang('title_field_to_modify'),
+			$mod->CreateInputDropdown($formDescriptor, 'opt_field_to_modify', $fieldlist, -1, $this->GetOption('field_to_modify')));
+
+		return array('main'=>$main);
+	}
+
+	function ModifyOtherFields()
+	{
+		if($this->Value !== FALSE)
+		{
+			$others = $this->formdata->Fields;
+			foreach($others as &$one)
+			{
+				if($one->IsDisposition()
+               		&& is_subclass_of($one,'pwfDispositionEmailBase')
+					&& $one->GetId() == $this->GetOption('field_to_modify'))
+				{
+					$cc = $one->GetOption('email_cc_address','');
+					if(!empty($cc))
+						$cc .= ',';
+					$cc .= $this->Value;
+					$one->SetOption('email_cc_address',$this->Value);
+				}
+			}
+			unset($one);
+		}
+	}
+
+	function StatusInfo()
+	{
+		$mod = $this->formdata->pwfmodule;
+		$others = $this->formdata->Fields;
+	  	foreach($others as &$one)
+		{
+			if($one->GetId() == $this->GetOption('field_to_modify'))
+			{
+				$ret = $mod->Lang('title_modifies',$one->GetName());
+				unset($one)
+				return $ret;
+			}
+		}
+		unset($one)
+	  	return $mod->Lang('unspecified');
+	}
+
+	function Validate()
+	{
+		$this->validated = TRUE;
+		$this->validationErrorText = '';
+		switch ($this->ValidationType)
+		{
+		 case 'email':
+			$mod = $this->formdata->pwfmodule;
+			if($this->Value !== FALSE &&
+				!preg_match($mod->email_regex, $this->Value))
+			{
+				$this->validated = FALSE;
+				$this->validationErrorText = $mod->Lang('please_enter_an_email',$this->Name);
+			}
+			break;
+		}
+		return array($this->validated, $this->validationErrorText);
+	}
+}
+
+?>
---
+++ lib/class.pwfSequenceEnd.php
@@ -0,0 +1,50 @@
+<?php
+# This file is part of CMS Made Simple module: PowerForms
+# Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
+# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Refer to licence and other details at the top of file PowerForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+
+class pwfSequenceEnd extends pwfFieldBase
+{
+	function __construct(&$formdata, &$params)
+	{
+		parent::__construct($formdata, $params);
+		$this->DisplayInSubmission = FALSE;
+		$this->NonRequirableField = TRUE;
+		$this->Type = 'SequenceEnd';
+		$this->sortable = FALSE;
+	}
+
+	function GetFieldInput($id, &$params, $returnid)
+	{
+		return '';
+	}
+
+	function StatusInfo()
+	{
+		return '';
+	}
+
+	function GetHumanReadableValue($as_string=TRUE)
+	{
+		// there's nothing human readable about a sequence of fields
+		$ret = '[End FieldSequence: '.$this->Value.']';
+		if($as_string)
+			return $ret;
+		else
+			return array($ret);
+	}
+
+	function PrePopulateAdminForm($formDescriptor)
+	{
+		$mod = $this->formdata->pwfmodule;
+		$main = array(
+			  array($mod->Lang('title_name'),
+					$mod->CreateInputText($formDescriptor,'opt_sequencename',
+					  $this->GetOption('legend',''), 50)));
+		return array('main'=>$main);
+	}
+}
+
+?>
