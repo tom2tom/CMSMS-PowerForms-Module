754e716b0472a2dcf5c7d2364b54f1a36f794666
tom2tom <tpgww@onepost.net>
2015-05-18 15:27:30 +1000

    replace code placeholders with working code

--- action.selected_forms.php
+++ action.selected_forms.php
@@ -4,31 +4,39 @@
 # Refer to licence and other details at the top of file PowerForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
 
-if (!empty($params['sel']))
+if(!empty($params['sel']))
 {
+	$funcs = new pwfFormOperations();
 	if(isset($params['clone']))
 	{
 		if(!$this->CheckAccess('ModifyPFForms')) exit;
-		foreach ($params['sel'] as $rid)
-		{
-			//do stuff
-		}
+		foreach ($params['sel'] as $fid)
+			$funcs->Copy($this,$fid,$params);
 	}
 	elseif(isset($params['delete']))
 	{
 		if(!$this->CheckAccess('ModifyPFForms')) exit;
-		foreach ($params['sel'] as $rid)
-		{
-			//do stuff
-		}
+		foreach ($params['sel'] as $fid)
+			$funcs->Delete($this,$fid);
 	}
 	elseif(isset($params['export']))
 	{
-		foreach ($params['sel'] as $rid)
+		$xmlstr = $funcs->CreateXML($this,$params['sel'],date('Y-m-d H:i:s'));
+		if($xmlstr)
 		{
-			//do stuff
+			@ob_clean();
+			@ob_clean();
+			header('Pragma: public');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			header('Cache-Control: private',FALSE);
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/force-download; charset=utf-8');
+			header('Content-Length: '.strlen($xmlstr));
+			header('Content-Disposition: attachment; filename='.$fn);
+			echo $xmlstr;
+			exit;
 		}
-		return;
 	}
 }
 
