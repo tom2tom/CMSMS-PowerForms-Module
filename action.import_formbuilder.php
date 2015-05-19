f77d13920d7a308ee9351ab754b3ac0fc5f5514e
tom2tom <tpgww@onepost.net>
2015-05-19 13:44:44 +1000

    update templates a bit ignore some formsoptions shorten aliases

--- action.import_formbuilder.php
+++ action.import_formbuilder.php
@@ -33,6 +33,115 @@ function Match_Browses(&$db,$pre)
 	}
 }
 
+/* REPLACEMENTS ...
+	$fld_X					$fld_Y
+	{$fb_form_header}		gone
+	{$fb_form_footer}		gone
+	$fb_*					$*
+	fbr_*					pwf_*
+	fb_invalid				fieldbad
+	FormBuilder				PowerForms
+	$in_formbrowser			$in_browser
+	$fbr_id					$browser_id
+	//see above $fb_hidden	$hidden
+	$sub_form_name			$form_name
+	$sub_url				$form_url
+	$sub_host				$form_host
+	$sub_source_ip			$sub_source
+	$fb_version				$version
+
+	$old_alias				$new_alias NEEDS manual update
+	{if ... $has_captcha .... {/if} NEEDS manual removal
+	e.g.
+	{if !empty($captcha_error)}
+		<div class="error_message">{$captcha_error}</div>
+	{/if}
+	or
+	{if !empty($has_captcha)}
+		<div class="captcha">{$graphic_captcha}{$title_captcha}<br />{$input_captcha}<br /></div>
+	{/if}
+*/
+function Update_Templates(&$mod,&$db,$pre,$oldfid,$newfid)
+{
+	$finds = array(
+		'FormBuilder',
+		'{$fb_form_header}',
+		'{$fb_form_footer}',
+		'$fb_version'
+		'fb_invalid',
+		'$fb_',
+		'$fbr_id',
+		'fbr_',
+		'$in_formbrowser',
+		'$sub_form_name',
+		'$sub_url',
+		'$sub_host',
+		'$sub_source_ip',
+	);
+	$repls = array(
+		'PowerForms',
+		'',
+		'',
+		'$version'
+		'fieldbad',
+		'$',
+		'$browser_id',
+		'pwf_',
+		'$in_browser',
+		'$form_name',
+		'$form_url',
+		'$form_host',
+		'$sub_source',
+	);
+
+	$sql = 'SELECT * FROM '.$pre.'module_pwf_trans WHERE NOT isform ORDER BY old_id';
+	$data = $db->GetAssoc($sql);
+	if($data)
+	{
+		foreach($data as &$row)
+		{
+			$finds[] = '\$fld_'.$row['old_id'];
+			$repls[] = '\$fld_'.$row['new_id'];
+		}
+		unset($row);
+	}
+
+	$tpl = $mod->GetTemplate('pwf_'.$newfid);
+	if($tpl)
+	{
+		$tpl = str_replace($finds,$repls,$tpl);
+		$mod->SetTemplate('pwf_'.$newfid,$tpl);
+	}
+
+	$sql = 'SELECT option_id,value FROM '.$pre.'module_pwf_form_opt WHERE form_id=? AND name = \'submission_template\'';
+	$row = $db->GetOne($sql,array($newfid));
+	if($row)
+	{
+		$sql = 'UPDATE '.$pre.'module_pwf_form_opt SET value=? WHERE option_id=?';
+		if($row['value'])
+		{
+			$tpl = str_replace($finds,$repls,$row['value']);
+			$db->Execute($sql,array($tpl,$row['option_id']));
+		}
+	}
+
+	$sql = 'SELECT option_id,value FROM '.$pre.'module_pwf_field_opt WHERE form_id=? AND name LIKE \'%template%\'';
+	$rows = $db->GetArray($sql,array($newfid));
+	if($rows)
+	{
+		$sql = 'UPDATE '.$pre.'module_pwf_field_opt SET value=? WHERE option_id=?';
+		foreach($rows as &$row)
+		{
+			if($row['value'])
+			{
+				$tpl = str_replace($finds,$repls,$row['value']);
+				$db->Execute($sql,array($tpl,$row['option_id']));
+			}
+		}
+		unset($row);
+	}
+}
+
 function Get_FieldOpts(&$db,$pre,$oldfid,$newfid,$oldf,$newf,&$fieldrow)
 {
 	$sql = 'SELECT * FROM '.$pre.'module_fb_field_opt WHERE form_id=? AND field_id=? ORDER BY option_id';
@@ -124,6 +233,10 @@ function Get_Opts(&$mod,&$db,$pre,$oldfid,$newfid)
 (option_id,form_id,name,value) VALUES (?,?,?,?)';
 		foreach($data as $row)
 		{
+			if(strpos($row['name'],'captcha') !== FALSE) //ignore redundant options
+				continue;
+			if(strpos($row['name'],'udt') !== FALSE && ($row['value'] == FALSE || $row['value'] == -1))
+				continue;
 			if($row['name'] == 'form_template')
 			{
 				$mod->SetTemplate('pwf_'.$newfid,$row['value']);
@@ -150,8 +263,10 @@ if(isset($params['import']))
 		{
 			$fid = $db->GenID($pre.'module_pwf_form_seq');
 			$alias = $row['alias'];
-			if(!$alias)
-				$alias = pwfUtils::MakeAlias();
+			if($alias)
+				$alias = pwfUtils::MakeAlias($alias,18); //maybe shorten
+			else
+				$alias = pwfUtils::MakeAlias($row['name'],18);
 			$ta = $alias;
 			$i = 1;
 			while(!$funcs->NewID(FALSE,$alias))
@@ -160,14 +275,15 @@ if(isset($params['import']))
 				$i++;
 			}
 			$db->Execute($sql,array($fid,$row['name'],$alias));
-			$renums[$fid] = (int)$row['form_id'];
+			$renums[(int)$row['form_id']] = $fid;
 		}
 		$sql = 'INSERT INTO '.$pre.'module_pwf_trans (old_id,new_id,isform) VALUES (?,?,1)';
-		foreach($renums as $new=>$old)
+		foreach($renums as $old=>$new)
 		{
 			$db->Execute($sql,array($old,$new));
 			Get_Opts($this,$db,$pre,$old,$new);
 			Get_Fields($db,$pre,$old,$new);
+			Update_Templates($this,$db,$pre,$old,$new);
 			//data may've already been imported by the browser module
 			$rs = $db->SelectLimit('SELECT * FROM '.$pre.'module_pwbr_browser',1);
 			if($rs)
