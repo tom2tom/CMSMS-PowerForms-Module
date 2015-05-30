f22a5299a7503a8fa13fe3ea9a3e1c11548edd7c
tom2tom <tpgww@onepost.net>
2015-05-30 17:15:36 +1000

    add method Get()

--- lib/class.pwfCache.php
+++ lib/class.pwfCache.php
@@ -8,7 +8,7 @@
 class pwfCache
 {
 	/**
-	Returns cache-object $module->cache, after creating it if not already done
+	Get:
 	@module: reference to current PowerForms module object
 	@storage: optional cache-type name, one (or more, ','-separated) of
 		auto,shmop,apc,memcached,wincache,xcache,memcache,redis,database
@@ -16,118 +16,60 @@ class pwfCache
 	@settings: optional array of general and cache-type-specific parameters,
 		(e.g. see default array in this func)
 		default empty
+	Returns: cache-object $module->cache (after creating it if not already done) or FALSE
 	*/
-	function __construct(&$module, $storage = 'auto', $settings = array())
+	public static function Get(&$module, $storage = 'auto', $settings = array())
 	{
-		if(!$module->cache)
+		if($module->cache)
+			return $module->cache;
+
+		$config = cmsms()->GetConfig();
+		$url = $config['root_url'];
+		$settings = array_merge(
+			array(
+				'memcache' => array(
+					array($url,11211,1)
+				),
+				'redis' => array(
+					'host' => $url,
+					'port' => '',
+					'password' => '',
+					'database' => '',
+					'timeout' => ''
+				),
+				'database' => array(
+					'table' => cms_db_prefix().'module_pwf_cache'
+				)
+			), $settings);
+
+		if($storage)
+			$storage = strtolower($storage);
+		else
+			$storage = 'auto';
+		if(strpos($storage,'auto') !== FALSE)
+			$storage = 'shmop,apc,memcached,wincache,xcache,memcache,redis,database';
+
+		$path = dirname(__FILE__).DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR;
+		require($path.'interface.FastCache.php');
+		require($path.'FastCacheBase.php');
+
+		$types = explode(',',$storage);
+		foreach($types as $one)
 		{
-			if($storage)
-				$settings['storage'] = $storage;
-
-			if (isset($settings['storage']
+			$one = trim($one);
+			require($path.$one.'.php');
+			$class = 'FastCache_'.$one;
+			try
 			{
-				if $settings['storage'])
-					$settings['storage'] = strtolower($settings['storage']);
-				else
-					unset($settings['storage']);
-			}
-			$config = cmsms()->GetConfig();
-			$url = $config['root_url'];
-			$settings = array_merge(
-				array(
-					'storage' => 'auto', // or '' == auto
-					'fallback' => 'database', // storage when nothing else is available
-					'memcache' => array(
-						array($url,11211,1)
-					),
-					'redis' => array(
-						'host' => $url,
-						'port' => '',
-						'password' => '',
-						'database' => '',
-						'timeout' => ''
-					)
-				), $settings);
-			if(strpos($settings['storage'],'auto') !== false)
-				$settings['storage'] = 'shmop,apc,memcached,wincache,xcache,memcache,redis,database';
-			$types = explode(',',$settings['storage']);
-
-			$type = false;
-			foreach($types as $one)
-			{
-				$one = trim($one);
-				switch($one)
-				{
-				 case 'apc':
-					if(extension_loaded('apc') && ini_get('apc.enabled') && strpos(PHP_SAPI,'CGI') === false)
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'database':
-					$type = $one;
-					break 2;
-				 case 'memcached':
-					if(class_exists('memcached'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'memcache':
-					if(function_exists('memcache_connect'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'redis':
-					if(class_exists('Redis'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'shmop':
-					if(extension_loaded('shmop'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'wincache':
-					if(extension_loaded('wincache') && function_exists('wincache_ucache_set'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				 case 'xcache':
-					if(extension_loaded('xcache') && function_exists('xcache_get'))
-					{
-						$type = $one;
-						break 2;
-					}
-					break;
-				}
-			}
-
-			if(!$type)
-				return false;
-			$path = dirname(__FILE__).DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR;
-			require($path.'interface.phpfastcache_driver.php');
-			require($path.'BasePhpFastCache.php');
-			require($path.$type.'.php');
-			$class = 'phpfastcache_'.$type;
-			$ob = new $class($settings);
-			if($ob)
+				$ob = new $class($settings);
 				$module->cache =& $ob;
-			else
-				return false;
+				return $module->cache;
+			}
+			catch(Exception $e) {}
 		}
-		return $module->cache;
+		return NULL;
 	}
+
 }
 
 ?>
