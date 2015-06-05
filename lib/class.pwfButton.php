e8429b0733d74d75ee260489926de8465b10458f
tom2tom <tpgww@onepost.net>
2015-06-05 14:00:13 +1000

    cleanup

--- lib/class.pwfButton.php
+++ lib/class.pwfButton.php
@@ -27,8 +27,9 @@ class pwfButton extends pwfFieldBase
 
 	function Populate($id,&$params)
 	{
-		return '<input type="button" name="'.$id.$this->formdata->current_prefix.$this->Id.
-		'" value="'.$this->GetOption('text').'"'.$this->GetIdTag().$this->GetScript().' />';
+		return '<input type="button" id="'.$this->GetInputId().'" name="'.
+			$id.$this->formdata->current_prefix.$this->Id.
+			'" value="'.$this->GetOption('text').'"'.$this->GetScript().' />';
 	}
 
 }
