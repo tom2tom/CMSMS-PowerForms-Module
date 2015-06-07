ea79065a4aaa1bc6c446a0d24feef5848d0fa3d5
tom2tom <tpgww@onepost.net>
2015-06-07 13:30:44 +1000

    cleanups

--- method.update_field.php
+++ method.update_field.php
@@ -10,38 +10,42 @@ if(!empty($message))
 $smarty->assign('backtomod_nav',$this->CreateLink($id,'defaultadmin','','&#171; '.$this->Lang('back_top')));
 $smarty->assign('backtoform_nav',$this->CreateLink($id,'update_form',$returnid,'&#171; '.$this->Lang('back_form'),array('formedit'=>1,'form_id'=>$params['form_id'])));
 
+$fid = $obfield->GetId();
+
 $smarty->assign('form_start',$this->CreateFormStart($id,'update_field',$returnid,
 	'POST','',FALSE,'',array(
 	'form_id'=>$params['form_id'],
 	'formdata'=>$params['formdata'],
-	'field_id'=>$obfield->GetId(),
+	'field_id'=>$fid,
 	'order_by'=>$obfield->GetOrder(), //TODO check used in pwfFieldBase::__construct()
 	'set_from_form'=>1))); //TODO check used in pwfFieldBase::__construct()
 $smarty->assign('form_end',$this->CreateFormEnd());
 
-$mainList = array();
-$advList = array();
-$baseList = $obfield->PrePopulateAdminFormCommon($id);
-/*if($obfield->GetFieldType() == '')
+$baseOpts = $obfield->PrePopulateAdminFormCommon($id);
+/*if($obfield->GetFieldType() == '') TODO relevant?
 {
 	// still need type
-	$fieldList = array();
+	$fieldOpts = array();
 }
 else
 {
 */
-	// we have our type
-	$fieldList = $obfield->PrePopulateAdminForm($id);
+	// type is known
+	$fieldOpts = $obfield->PrePopulateAdminForm($id);
 //}
 
-$hasmain = isset($baseList['main']) || isset($fieldList['main']);
-$hasadvanced = isset($baseList['adv']) || isset($fieldList['adv']);
+$allOpts = array_merge_recursive($baseOpts,$fieldOpts);
+if(!isset($allOpts['main']) $allOpts['main'] = array();
+if(!isset($allOpts['adv']) $allOpts['adv'] = array();
+$obfield->PostPopulateAdminForm($allOpts['main'],$allOpts['adv']);
+$hasmain = (count($allOpts['main']) > 0);
+$hasadv = (count($allOpts['adv']) > 0);
 
 $tab = $this->GetActiveTab($params);
 $t = $this->StartTabHeaders();
 if($hasmain)
 	$t .= $this->SetTabHeader('maintab',$this->Lang('tab_field'),($tab == 'maintab'));
-if($hasadvanced)
+if($hasadv)
 	$t .= $this->SetTabHeader('advancedtab',$this->Lang('tab_advanced'),($tab == 'advancedtab'));
 $t .= $this->EndTabHeaders().$this->StartTabContent();
 
@@ -49,20 +53,16 @@ $smarty->assign('tabs_start',$t);
 $smarty->assign('tabs_end',$this->EndTabContent());
 if($hasmain)
 	$smarty->assign('maintab_start',$this->StartTab('maintab'));
-if($hasadvanced)
+if($hasadv)
 	$smarty->assign('advancedtab_start',$this->StartTab('advancedtab'));
 $smarty->assign('tab_end',$this->EndTab());
 
-if($obfield->GetId())
-{
-	$smarty->assign('op',$this->CreateInputHidden($id,'field_op',$this->Lang('updated')));
-	$smarty->assign('submit',$this->CreateInputSubmit($id,'fieldupdate',$this->Lang('update')));
-}
-elseif($obfield->GetFieldType())
-{
-	$smarty->assign('op',$this->CreateInputHidden($id,'field_op',$this->Lang('added')));
-	$smarty->assign('submit',$this->CreateInputSubmit($id,'fieldadd',$this->Lang('add')));
-}
+if($fid > 0) //editing existing field
+	$t = $this->CreateInputSubmit($id,'fieldupdate',$this->Lang('update')));
+else //if($obfield->GetFieldType())
+	$t = $this->CreateInputSubmit($id,'fieldadd',$this->Lang('add')));
+
+$smarty->assign('submit',$t);
 $smarty->assign('cancel',$this->CreateInputSubmit($id,'cancel',$this->Lang('cancel')));
 
 $t = ($obfield->HasAddOp())?
@@ -76,94 +76,65 @@ $smarty->assign('del',$t);
 $t = (/*!$obfield->IsDisposition() && */!$obfield->IsNonRequirableField())?1:0;
 $smarty->assign('requirable',$t);
 
-if(isset($baseList['main']))
+$mainList = array();
+if($hasmain)
 {
-	foreach($baseList['main'] as $item)
+	foreach($allOpts['main'] as $item)
 	{
-		list($titleStr,$inputStr,$helpStr) = $item + array(NULL,NULL,NULL);
 		$oneset = new stdClass();
-		if($titleStr) $oneset->title = $titleStr;
-		if($inputStr) $oneset->input = $inputStr;
-		if($helpStr) $oneset->help = $helpStr;
+		$oneset->title = (isset($item[0]))?$item[0]:'';
+		$oneset->input = (isset($item[1]))?$item[1]:'';
+		if(!empty($item[2])) $oneset->help = $item[2]; //optional
 		$mainList[] = $oneset;
 	}
 }
-if(isset($baseList['adv']))
-{
-	foreach($baseList['adv'] as $item)
-	{
-		list($titleStr,$inputStr,$helpStr) = $item + array(NULL,NULL,NULL);
-		$oneset = new stdClass();
-		if($titleStr) $oneset->title = $titleStr;
-		if($inputStr) $oneset->input = $inputStr;
-		if($helpStr) $oneset->help = $helpStr;
-		$advList[] = $oneset;
-	}
-}
-if(isset($fieldList['main']))
-{
-	foreach($fieldList['main'] as $item)
-	{
-		list($titleStr,$inputStr,$helpStr) = $item + array(NULL,NULL,NULL);
-		$oneset = new stdClass();
-		if($titleStr) $oneset->title = $titleStr;
-		if($inputStr) $oneset->input = $inputStr;
-		if($helpStr) $oneset->help = $helpStr;
-		$mainList[] = $oneset;
-	}
-}
-if(isset($fieldList['adv']))
-{
-	foreach($fieldList['adv'] as $item)
-	{
-		list($titleStr,$inputStr,$helpStr) = $item + array(NULL,NULL,NULL);
-		$oneset = new stdClass();
-		if($titleStr) $oneset->title = $titleStr;
-		if($inputStr) $oneset->input = $inputStr;
-		if($helpStr) $oneset->help = $helpStr;
-		$advList[] = $oneset;
-	}
-}
-$obfield->PostPopulateAdminForm($mainList,$advList);
-if(!$advList)
-{
-	$oneset = new stdClass();
-	$oneset->help = $this->Lang('title_no_advanced_options');
-	$advList[] = $oneset;
-//TODO no advanced vars help for page
-}
-
 $smarty->assign('mainList',$mainList);
-$smarty->assign('advList',$advList);
-if(isset($fieldList['table']))
-	$smarty->assign('mainTable',$fieldList['table']);
-else
-	$smarty->clear_assign('mainTable');
-if(isset($fieldList['funcs']))
-	$smarty->assign('jsfuncs',$fieldList['funcs']);
-else
-	$smarty->clear_assign('jsfuncs');
 
-if(isset($fieldList['extra']))
+$advList = array();
+if($hasadv)
 {
-	switch($fieldList['extra'])
+	foreach($allOpts['adv'] as $item)
 	{
-	 case 'varshelpadv':
-		$smarty->assign('advvarhelp',1);
-		pwfUtils::SetupFormVarsHelp($this,$smarty,$formdata->Fields);
-		break;
+		$oneset = new stdClass();
+		$oneset->title = (isset($item[0]))?$item[0]:'';
+		$oneset->input = (isset($item[1]))?$item[1]:'';
+		if(!empty($item[2])) $oneset->help = $item[2]; //optional
+		$advList[] = $oneset;
+	}
+}
+$smarty->assign('advList',$advList);
+
+if(isset($allOpts['table']))
+	$smarty->assign('mainTable',$allOpts['table']);
+if(isset($allOpts['funcs']))
+	$smarty->assign('jsfuncs',$allOpts['funcs']);
+$smarty->assign('incpath',$this->GetModuleURLPath().'/include/');
+
+if(isset($allOpts['extra']))
+{
+	switch($allOpts['extra'])
+	{
 	 case 'varshelpmain':
-		$smarty->assign('mainvarhelp',1);
+		if($hasmain)
+			$smarty->assign('mainvarhelp',1);
+		break;
+	 case 'varshelpadv':
+		if($hasadv)
+		{
+			$smarty->assign('advvarhelp',1);
+			pwfUtils::SetupFormVarsHelp($this,$smarty,$formdata->Fields);
+		}
 		break;
 	 case 'varshelpboth':
-		$smarty->assign('mainvarhelp',1);
-		$smarty->assign('advvarhelp',1);
-		pwfUtils::SetupFormVarsHelp($this,$smarty,$formdata->Fields);
+		if($hasmain)
+			$smarty->assign('mainvarhelp',1);
+		if($hasadv)
+		{
+			$smarty->assign('advvarhelp',1);
+			pwfUtils::SetupFormVarsHelp($this,$smarty,$formdata->Fields);
+		}
 		break;
 	}
-
 }
-
-$smarty->assign('incpath',$this->GetModuleURLPath().'/include/');
 
 ?>
