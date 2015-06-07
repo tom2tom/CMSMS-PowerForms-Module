81ec7de15493ec6fa8cb95b6236317c58aff9e68
tom2tom <tpgww@onepost.net>
2015-06-07 11:17:53 +1000

    use cached formdata

--- action.update_field.php
+++ action.update_field.php
@@ -8,18 +8,32 @@
 if(!$this->CheckAccess('ModifyPFForms')) exit;
 
 if(isset($params['cancel']))
-	$this->Redirect($id,'update_form',$returnid,
-		array('formedit'=>1,'form_id'=>$params['form_id'],'active_tab'=>'fieldstab'));
+	$this->Redirect($id,'update_form',$returnid,array(
+		'formedit'=>1,
+		'form_id'=>$params['form_id'],
+		'formdata'=>$params['formdata'],
+		'active_tab'=>'fieldstab'));
 
-$funcs = new pwfFieldOperations();
-$formdata = $this->GetFormData($params);
-$obfield = $funcs->NewField($formdata,$params);
-unset($funcs);
+$cache = pwfCache::Get($this);
+if(isset($params['formdata']))
+{
+	$formdata = unserialize($cache->driver_get($params['formdata']));
+	$formdata->formsmodule =& $this;
+//	$cache->driver_delete($params['formdata']);
+}
+else
+{
+	//should never happen
+$this->Crash();
+//	$params['formdata'] = md5(uniqid().mt_rand(10,10000010));
+//	$formdata = $this->GetFormData($params);
+}
+$obfield = pwfFieldOperations::NewField($formdata,$id,$params);
 
 $message = '';
 
 if(isset($params['fieldupdate']) ||
-	(isset($params['fieldadd']) && $obfield->GetFieldType()))
+  (isset($params['fieldadd']) && $obfield->GetFieldType()))
 {
 	$obfield->PostAdminSubmitCleanup($params);
 	$val = $obfield->AdminValidate($id);
@@ -28,13 +42,20 @@ if(isset($params['fieldupdate']) ||
 		$obfield->Store(TRUE);
 		$obfield->PostFieldSaveProcess($params);
 		$message = $this->Lang('field_op',$params['field_op']);
+		//update cache ready for next use
+		$formdata->formsmodule = NULL;
+		$cache->driver_set($params['formdata'],serialize($formdata));
 		$this->Redirect($id,'update_form',$returnid,array(
-			'formedit'=>1,'form_id'=>$params['form_id'],'active_tab'=>'fieldstab',
+			'formedit'=>1,
+			'form_id'=>$params['form_id'],
+			'formdata'=>$params['formdata'],
+			'active_tab'=>'fieldstab',
 			'message'=>	$this->PrettyMessage($message,TRUE,FALSE,FALSE)));
 	}
 	else
 	{
-		$obfield->LoadField($params);
+		//start again
+		$obfield->Load($id,$params); //TODO check for failure
 		$message = $this->PrettyMessage($val[1],FALSE,FALSE,FALSE);
 	}
 }
@@ -59,6 +80,9 @@ else
 }
 
 require dirname(__FILE__).DIRECTORY_SEPARATOR.'method.update_field.php';
+
+$formdata->formsmodule = NULL; //no need to cache this
+$cache->driver_set($params['formdata'],serialize($formdata));
 
 echo $this->ProcessTemplate('editfield.tpl');
 
