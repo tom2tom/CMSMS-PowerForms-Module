276ef7722aae0e5ebc9d0377f831a3e8c53c549d
tom2tom <tpgww@onepost.net>
2016-01-02 13:04:35 +1100

    working admin code

--- lib/class.pwfSequenceEnd.php
+++ lib/class.pwfSequenceEnd.php
@@ -1,7 +1,6 @@
 <?php
 # This file is part of CMS Made Simple module: PowerForms
-# Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Copyright (C) 2015-2016 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file PowerForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
 
@@ -11,15 +10,17 @@ class pwfSequenceEnd extends pwfFieldBase
 	{
 		parent::__construct($formdata, $params);
 		$this->ChangeRequirement = FALSE;
-		$this->DisplayInForm = FALSE;
+		$this->DisplayInForm = FALSE; //all handled by SequenceStart
 		$this->DisplayInSubmission = FALSE;
+		$this->HasLabel = FALSE;
 		$this->IsSortable = FALSE;
+		$this->NeedsDiv = FALSE;
 		$this->Type = 'SequenceEnd';
 	}
 
 	function GetHumanReadableValue($as_string=TRUE)
 	{
-		$ret = '[End FieldSequence: '.$this->Value.']';
+		$ret = '[End FieldSequence: '.$this->GetOption('starter').']';
 		if($as_string)
 			return $ret;
 		else
@@ -30,14 +31,42 @@ class pwfSequenceEnd extends pwfFieldBase
 	{
 		list($main,$adv) = $this->AdminPopulateCommon($id);
 		$mod = $this->formdata->formsmodule;
-		//TODO select sequence start field
-/*		$main[] = array($mod->Lang(''),
-						$mod->CreateInputDropdown($id,'opt_starter',
-							$this->GetOption('starter'),50));
+		$this->RemoveAdminField($main,$mod->Lang('title_field_alias'));
+		$this->RemoveAdminField($main,$mod->Lang('title_field_validation'));
+		$this->RemoveAdminField($main,$mod->Lang('title_field_helptext'));
+/*		$choices = array of current starter fields + notyet
+		$main[] = array($mod->Lang(''),
+						$mod->CreateInputDropdown($id,'opt_starter',$choices,
+							-1,$this->GetOption('starter')));
 */
-		return array('main'=>$main,'adv'=>$adv);
+		$main[] = array($mod->Lang('title_privatename'),
+						$mod->CreateInputText($id,'opt_privatename',
+							$this->GetOption('privatename',htmlentities($this->Name)),20,50));
+
+		$main[] = array($mod->Lang('title_startername'),
+						$mod->CreateInputText($id,'opt_starter',
+							$this->GetOption('starter'),20,50));
+		return array('main'=>$main,'adv'=>array());
 	}
 
+/*	function PostAdminAction(&$params)
+	{
+		'starter' option = that_field ->Options['name'], not ->Name
+		conform any starterfield::opt_ender
+	}
+
+	function AdminValidate($id)
+	{
+		unique privatenamename
+		warn if startername not found
+		warn if order is not after corresponding sequence start
+	}
+*/
+/*	function Populate($id,&$params)
+	{
+		return '</div><!-- end sequence '.$this->GetOption('starter').' -->';
+	}
+*/
 }
 
 ?>
--- lib/class.pwfSequenceStart.php
+++ lib/class.pwfSequenceStart.php
@@ -1,7 +1,6 @@
 <?php
 # This file is part of CMS Made Simple module: PowerForms
-# Copyright (C) 2012-2015 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Copyright (C) 2015-2016 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file PowerForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
 
@@ -11,15 +10,19 @@ class pwfSequenceStart extends pwfFieldBase
 	{
 		parent::__construct($formdata, $params);
 		$this->ChangeRequirement = FALSE;
-		$this->DisplayInForm = FALSE;
 		$this->DisplayInSubmission = FALSE;
+		$this->HasLabel = FALSE;
+//?		$this->HasUserAddOp = TRUE;
+//?		$this->HasUserDeleteOp = TRUE;
 		$this->IsSortable = FALSE;
+//?		$this->MultiPopulate = TRUE;
+		$this->NeedsDiv = FALSE;
 		$this->Type = 'SequenceStart';
 	}
 
 	function GetHumanReadableValue($as_string=TRUE)
 	{
-		$ret = '[Begin FieldSequence: '.$this->Value.']';
+		$ret = '[Begin FieldSequence: '.$this->GetOption('privatename').']';
 		if($as_string)
 			return $ret;
 		else
@@ -30,14 +33,80 @@ class pwfSequenceStart extends pwfFieldBase
 	{
 		list($main,$adv) = $this->AdminPopulateCommon($id);
 		$mod = $this->formdata->formsmodule;
-		$main[] = array($mod->Lang('title_name'),
-						$mod->CreateInputText($id,'opt_sequencename',
-							$this->GetOption('legend'),50));
+		$this->RemoveAdminField($main,$mod->Lang('title_field_validation'));
+		$this->RemoveAdminField($main,$mod->Lang('title_field_helptext'));
+
+		$main[] = array($mod->Lang('title_privatename'),
+						$mod->CreateInputText($id,'opt_privatename',
+							$this->GetOption('privatename',htmlentities($this->Name)),20,50));
+		//paired end
+/*		$choices = array of current ender fields + notyet
+		$main[] = array($mod->Lang(''),
+						$mod->CreateInputDropdown($id,'opt_starter',$choices,
+							-1,$this->GetOption('starter')));
+*/
+		$main[] = array($mod->Lang('title_endername'),
+						$mod->CreateInputText($id,'opt_ender',
+							$this->GetOption('ender'),20,50));
+		$main[] = array($mod->Lang('title_initial_count'),
+						$mod->CreateInputText($id,'opt_repeatcount',
+							$this->GetOption('repeatcount',1),2,2));
+		$main[] = array($mod->Lang('title_max_count'),
+						$mod->CreateInputText($id,'opt_maxcount',
+							$this->GetOption('maxcount',0),2,2));
+
+		$this->RemoveAdminField($adv,$mod->Lang('title_field_javascript'));
+		$this->RemoveAdminField($adv,$mod->Lang('title_field_resources'));
 
 		return array('main'=>$main,'adv'=>$adv);
-		//TODO repeatcount
 	}
 
+/*	function PostAdminAction(&$params)
+	{
+		conform any enderfield::opt_starter
+	}
+
+	function AdminValidate($id)
+	{
+		TODO
+		unique privatename
+		max adds 0..20?
+		repeatcount 1..Max
+	}
+*/
+	function Populate($id,&$params)
+	{
+		/*
+		setup js:
+		$(BTNSHOW).css('display','none').click({
+			$(DIV).css('display','block');
+			$(this).css('display','none');
+			$(BTNHIDE).css('display','inline-block');
+		});
+		$(BTNHIDE).click({
+			$(DIV).css('display','none');
+			$(this).css('display','none');
+			$(BTNSHOW).css('display','inline-block');
+		});
+		if only 1 sequence
+			$(BTNREM).css('display','none');
+		*/
+		$name = $this->GetOption('privatename'); //htmlentities?
+		$repeats = $this->GetOption('repeatcount');
+		$ret = '';
+		for ($i=0; $i<$repeats; $i++) //OR support looping in form constructor
+		{
+			$ret = 'BTNHIDE BTNSHOW BTNADD BTNREM';
+			$ret .= '<br />'.htmlentities('>>>>').PHP_EOL;
+			$ret .= '<div id="'.$name.'" class="sequence"><!-- start sequence '.$name.' -->'.PHP_EOL;
+			//FIELD(S) STUFF
+			//?? HOWTO if NESTED ???
+			$ret .= '</div><!-- end sequence '.$name.' -->'.PHP_EOL;
+			$ret .= htmlentities('<<<<').PHP_EOL;
+		}
+		return $ret;
+		//TODO skip $formdata->Fields to relevant SequenceEnd
+	}
 }
 
 ?>
