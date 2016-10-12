22c31831b199ef969c57d60f1d446e71c1c1f699
tom2tom <tpgww@onepost.net>
2016-10-12 15:03:12 +1100

    placeholder for show_form action

---
+++ action.show_form.php
@@ -0,0 +1,14 @@
+<?php
+# This file is part of CMS Made Simple module: PWForms
+# Copyright (C) 2012-2016 Tom Phane <tpgww@onepost.net>
+# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Refer to licence and other details at the top of file PWForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+
+/*TODO as per action.default except $params include:
+'cancel' if present show a cancel-button
+'resume' action name for redirect after completion/cancel
+'values' optional array of initial content for relevant form-field(s) as fieldid/name=>singleton or array
+'notfields' optional array of id's and/or aliases to omit if in form
+'nottypes' optional array of field-types to omit if in form
+*/
