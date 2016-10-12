3fe68a42a778d3058fe67945aefdfccb9e1a0c6b
tom2tom <tpgww@onepost.net>
2016-10-13 08:19:03 +1100

    8-byte integer fields create uploads dir with permissions 777

--- method.install.php
+++ method.install.php
@@ -17,97 +17,97 @@ $taboptarray = array('mysql' => 'ENGINE MyISAM CHARACTER SET utf8 COLLATE utf8_g
 'mysqli' => 'ENGINE MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci');
 $dict = NewDataDictionary($db);
 
-$flds = "
-form_id I KEY,
+$flds = '
+form_id I(8) KEY,
 name C(256),
 alias C(32)
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_form',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
 $db->CreateSequence($pre.'module_pwf_form_seq');
 $db->Execute('create index '.$pre.'module_pwf_form_idx on '.$pre.'module_pwf_form (alias)');
 
-$flds = "
-option_id I KEY,
-form_id I,
+$flds = '
+option_id I(8) KEY,
+form_id I(8),
 name C(64),
 value X
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_form_opt',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
 $db->CreateSequence($pre.'module_pwf_form_opt_seq');
 $db->Execute('create index '.$pre.'module_pwf_form_opt_idx on '.$pre.'module_pwf_form_opt (form_id)');
 
-$flds = "
-field_id I KEY,
-form_id I,
+$flds = '
+field_id I(8) KEY,
+form_id I(8),
 name C(96),
 type C(48),
 order_by I(2)
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_field',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
 $db->CreateSequence($pre.'module_pwf_field_seq');
 $db->Execute('CREATE INDEX '.$pre.'module_pwf_field_idx ON '.$pre.'module_pwf_field (form_id)');
 
-$flds = "
-option_id I KEY,
-field_id I,
-form_id I,
+$flds = '
+option_id I(8) KEY,
+field_id I(8),
+form_id I(8),
 name C(256),
 value X
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_field_opt',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
 $db->CreateSequence($pre.'module_pwf_field_opt_seq');
 $db->Execute('CREATE INDEX '.$pre.'module_pwf_field_opt_idx ON '.$pre.'module_pwf_field_opt (field_id,form_id)');
 
-$flds = "
+$flds = '
 record_id I(4) KEY,
 pubkey C(16),
-submitted ".CMS_ADODB_DT.",
+submitted '.CMS_ADODB_DT.',
 content B
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_record',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
 $db->CreateSequence($pre.'module_pwf_record_seq');
 
 /*MUTEX
-$flds = "
+$flds = '
 flock_id I(8) KEY,
 flock T
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_flock',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 */
 
-$flds = "
+$flds = '
 cache_id I(2) AUTO KEY,
 keyword C(48),
 value B,
-save_time ".CMS_ADODB_DT;
+save_time '.CMS_ADODB_DT;
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_cache',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
-$flds = "
+$flds = '
 log_id I(2) KEY AUTO,
 src C(40),
 howmany I(2) DEFAULT 1,
-basetime ".CMS_ADODB_DT;
+basetime '.CMS_ADODB_DT;
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_ip_log',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
-$flds = "
+$flds = '
 trans_id I(2) AUTO KEY,
 old_id I(2),
 new_id I(2),
 isform I(1)
-";
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_pwf_trans',$flds,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
@@ -127,7 +127,7 @@ $fp = $config['uploads_path'];
 if ($fp && is_dir($fp)) {
 	$ud = $this->GetName();
 	$fp = $fp.DIRECTORY_SEPARATOR.$ud;
-	if (!(is_dir($fp) || mkdir($fp,0644)))
+	if (!(is_dir($fp) || mkdir($fp,0777,TRUE))) //don't know how server is running!
 		$ud = '';
 } else
 	$ud = '';
