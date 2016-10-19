268c23119bcf30665d246562a45b5114b52fcb6d
tom2tom <tpgww@onepost.net>
2016-10-19 17:17:22 +1100

    conform renamed class-methods changed admin-object name-prefix remove validation

--- lib/class.Checkbox.php
+++ lib/class.Checkbox.php
@@ -13,19 +13,17 @@ class Checkbox extends FieldBase
 	{
 		parent::__construct($formdata,$params);
 		$this->IsInput = TRUE;
-		$this->Type =  'Checkbox';
-		$mod = $formdata->formsmodule;
-		$this->ValidationTypes = array(
-		$mod->Lang('validation_none')=>'none',
-		$mod->Lang('validation_must_check')=>'checked');
+		$this->Type = 'Checkbox';
 	}
 
 	public function GetFieldStatus()
 	{
 		$mod = $this->formdata->formsmodule;
-		$ret = ($this->GetOption('is_checked',0)?$mod->Lang('checked_by_default'):$mod->Lang('unchecked_by_default'));
-		if (strlen($this->ValidationType) > 0)
-		  	$ret .= ",".array_search($this->ValidationType,$this->ValidationTypes);
+		$ret = ($this->GetProperty('is_checked',0)?$mod->Lang('checked_by_default'):$mod->Lang('unchecked_by_default'));
+		if ($this->ValidationType) {
+			$this->EnsureArray($this->ValidationTypes);
+			$ret .= ','.array_search($this->ValidationType,$this->ValidationTypes);
+		}
 
 		return $ret;
 	}
@@ -34,9 +32,9 @@ class Checkbox extends FieldBase
 	{
 		$mod = $this->formdata->formsmodule;
 		if (!property_exists($this,'Value') || !$this->Value)
-			$ret = $this->GetOption('unchecked_value',$mod->Lang('value_unchecked'));
+			$ret = $this->GetProperty('unchecked_value',$mod->Lang('value_unchecked'));
 		else
-			$ret = $this->GetOption('checked_value',$mod->Lang('value_checked'));
+			$ret = $this->GetProperty('checked_value',$mod->Lang('value_checked'));
 
 		if ($as_string)
 			return $ret;
@@ -46,27 +44,27 @@ class Checkbox extends FieldBase
 
 	public function AdminPopulate($id)
 	{
-		list($main,$adv) = $this->AdminPopulateCommon($id);
+		list($main,$adv) = $this->AdminPopulateCommon($id,TRUE);
 		$mod = $this->formdata->formsmodule;
 		$main[] = array($mod->Lang('title_checkbox_label'),
-						$mod->CreateInputText($id,'opt_label',
-							$this->GetOption('label'),25,255));
+						$mod->CreateInputText($id,'pdt_label',
+							$this->GetProperty('label'),25,255));
 		$main[] = array($mod->Lang('title_checked_value'),
-						$mod->CreateInputText($id,'opt_checked_value',
-							$this->GetOption('checked_value',$mod->Lang('value_checked')),25,255));
+						$mod->CreateInputText($id,'pdt_checked_value',
+							$this->GetProperty('checked_value',$mod->Lang('value_checked')),25,255));
 		$main[] = array($mod->Lang('title_unchecked_value'),
-						$mod->CreateInputText($id,'opt_unchecked_value',
-							$this->GetOption('unchecked_value',$mod->Lang('value_unchecked')),25,255));
+						$mod->CreateInputText($id,'pdt_unchecked_value',
+							$this->GetProperty('unchecked_value',$mod->Lang('value_unchecked')),25,255));
 		$main[] = array($mod->Lang('title_default_set'),
-						$mod->CreateInputHidden($id,'opt_is_checked',0).
-						$mod->CreateInputCheckbox($id,'opt_is_checked',1,
-							$this->GetOption('is_checked',0)));
+						$mod->CreateInputHidden($id,'pdt_is_checked',0).
+						$mod->CreateInputCheckbox($id,'pdt_is_checked',1,
+							$this->GetProperty('is_checked',0)));
 		return array('main'=>$main,'adv'=>$adv);
 	}
 
 	public function Populate($id,&$params)
 	{
-		if ((!property_exists($this,'Value') || !$this->Value) && $this->GetOption('is_checked',0))
+		if ((!property_exists($this,'Value') || !$this->Value) && $this->GetProperty('is_checked',0))
 			$this->Value = 't';
 
 		$tid = $this->GetInputId();
@@ -74,31 +72,11 @@ class Checkbox extends FieldBase
 			$id,$this->formdata->current_prefix.$this->Id,'t',$this->Value,
 			'id="'.$tid.'"'.$this->GetScript());
 		$tmp = $this->SetClass($tmp);
-		$label = $this->GetOption('label');
+		$label = $this->GetProperty('label');
 		if ($label) {
 			$label = '<label for="'.$tid.'">'.$label.'</label>';
 			$label = '&nbsp;'.$this->SetClass($label);
 		}
 		return $tmp.$label;
-	}
-
-	public function Validate($id)
-	{
-		$this->valid = TRUE;
-		$this->ValidationMessage = '';
-
-		switch ($this->ValidationType) {
-		 case 'checked':
-			if (!property_exists($this,'Value') || !$this->Value) {
-				$this->valid = FALSE;
-				$mod = $this->formdata->formsmodule;
-				$label = $this->GetOption('label',$mod->Lang('thebox'));
-				$this->ValidationMessage = $mod->Lang('you_must_check',$label);
-			}
-			break;
-		 default:
-			break;
-		}
-		return array($this->valid,$this->ValidationMessage);
 	}
 }
