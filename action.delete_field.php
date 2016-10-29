83cb5d7bb2d0e23defea9ac79b60f2bfb448b9e5
tom2tom <tpgww@onepost.net>
2016-10-29 20:02:43 +1100

    add ajax-processors

---
+++ action.delete_field.php
@@ -0,0 +1,29 @@
+<?php
+# This file is part of CMS Made Simple module: PWForms
+# Copyright (C) 2012-2016 Tom Phane <tpgww@onepost.net>
+# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Refer to licence and other details at the top of file PWForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+
+//this action processes ajax-calls
+
+if (isset($params['formdata'])) {
+	try {
+		$cache = PWForms\Utils::GetCache($this);
+	} catch (Exception $e) {
+		echo '0';
+		exit;
+	}
+	$formdata = $cache->get($params['formdata']);
+	if (is_null($formdata) || !$formdata->Fields) {
+		echo '0';
+		exit;
+	}
+}
+
+//PWForms\FieldOperations::DeleteField($formdata,$params['field_id']);
+unset($formdata->Fields[$field_id]);
+$cache->set($params['formdata'],$formdata,84600);
+
+echo '1';
+exit;
---
+++ action.require_field.php
@@ -0,0 +1,32 @@
+<?php
+# This file is part of CMS Made Simple module: PWForms
+# Copyright (C) 2012-2016 Tom Phane <tpgww@onepost.net>
+# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
+# Refer to licence and other details at the top of file PWForms.module.php
+# More info at http://dev.cmsmadesimple.org/projects/powerforms
+
+//this action processes ajax-calls
+
+if (isset($params['formdata'])) {
+	try {
+		$cache = PWForms\Utils::GetCache($this);
+	} catch (Exception $e) {
+		echo '0';
+		exit;
+	}
+	$formdata = $cache->get($params['formdata']);
+	if (is_null($formdata) || !$formdata->Fields) {
+		echo '0';
+		exit;
+	}
+}
+
+$obfield = $formdata->Fields[$params['field_id']];
+if ($obfield !== FALSE) {
+	$obfield->SetRequired(($params['reqd']=='on'));
+//	$obfield->Store();
+	$cache->set($params['formdata'],$formdata,84600);
+}
+
+echo '1';
+exit;
