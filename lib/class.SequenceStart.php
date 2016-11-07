36ff0dd766d09893caeed9ad363f53c3409cdfbf
tom2tom <tpgww@onepost.net>
2016-11-07 13:42:13 +1100

    major rewrite

--- lib/class.SequenceStart.php
+++ lib/class.SequenceStart.php
@@ -8,65 +8,132 @@ namespace PWForms;
 
 class SequenceStart extends FieldBase
 {
+	public $IsSequence = TRUE;
+	public $Repeats = 1;
+
 	public function __construct(&$formdata, &$params)
 	{
 		parent::__construct($formdata, $params);
-		$this->ChangeRequirement = FALSE;
 		$this->DisplayInSubmission = FALSE;
-		$this->HasLabel = FALSE;
-//?		$this->HasUserAddOp = TRUE;
-//?		$this->HasUserDeleteOp = TRUE;
-//?		$this->MultiPopulate = TRUE;
+		$this->HasLabel = TRUE;
+		//name/label not user-modifiable for this class
+		$this->Name = $formdata->formsmodule->Lang('sequence_start').' &#187;&#187;'; //right angle quotes
 		$this->NeedsDiv = FALSE;
 		$this->Type = 'SequenceStart';
 	}
 
-	public function GetDisplayableValue($as_string=TRUE)
+	protected function CountSequences(){
+		$nm = $this->GetProperty('privatename'); //sequence identifier
+		$count = 0;
+		$ender = NULL;
+		foreach ($this->formdata->Fields as $obfld) { //TODO per field-orders
+			if ($obfld->Type == 'SequenceEnd') {
+				if ($obfld->GetProperty('privatename') == $nm) { //belongs to this sequence
+					$ender = $obfld;
+					$obfld->SetLast(FALSE);
+					$count++;
+				}
+			}
+		}
+		if ($ender) {
+			$ender->SetLast(TRUE);
+		}
+		$this->Repeats = $count;
+		return $count;
+	}
+
+	protected function CopySequenceFields()
 	{
-		$ret = '[Begin FieldSequence: '.$this->GetProperty('privatename').']';
-		if ($as_string)
-			return $ret;
-		else
-			return array($ret);
+		$c = $this->GetProperty('maxcount');
+		if ($c > 0) {
+			$cn = $this->CountSequences();
+			if ($cn > $c)
+				return; //TODO advice for user
+		}
+		foreach ($this->formdata->Fields as $obfld) {
+			if ($obfld == $this) {
+/*
+	note position
+  find next break
+  get all intemediate fields
+  append break field
+  'insert' after/before this position (append?)
+  update $LastBreak's
+  update orders
+*/
+			}
+		}
+	}
+
+	private function DeleteSequenceFields()
+	{
+		$c = $this->GetProperty('mincount');
+		if ($c > 1) {
+			$cn = $this->CountSequences();
+			if ($cn <= $c)
+				return; //TODO advice for user
+		}
+		foreach ($this->formdata->Fields as $obfld) {
+			if ($obfld == $this) {
+/*
+	note position
+  find next break
+  get all intemediate fields
+    delete them
+   if following break field
+    delete that
+   update $LastBreak's
+   update orders 
+*/
+			}
+		}
+	}
+
+	public function GetSynopsis()
+	{
+		$t = $this->formdata->formsmodule->Lang('title_privatename');
+		return $t.': '.$this->GetProperty('privatename');
 	}
 
 	public function AdminPopulate($id)
 	{
-		list($main,$adv) = $this->AdminPopulateCommon($id,TRUE); // !$visible?
+		$except = array(
+		'title_field_name',
+		'title_field_alias',
+		'title_field_helptext',
+		'title_field_javascript',
+		'title_field_resources',
+		'title_smarty_eval');
+		list($main,$adv) = $this->AdminPopulateCommon($id,$except,TRUE); // !$visible?
 		$mod = $this->formdata->formsmodule;
-		$this->RemoveAdminField($main,$mod->Lang('title_field_helptext'));
 
+		$alias = $this->ForceAlias(htmlentities($this->Name));
 		$main[] = array($mod->Lang('title_privatename'),
-						$mod->CreateInputText($id,'pdt_privatename',
-							$this->GetProperty('privatename',htmlentities($this->Name)),20,50));
-		//paired end
-/*		$choices = array of current ender fields + notyet
-		$main[] = array($mod->Lang(''),
-						$mod->CreateInputDropdown($id,'pdt_starter',$choices,
-							-1,$this->GetProperty('starter')));
-*/
-		$main[] = array($mod->Lang('title_endername'),
-						$mod->CreateInputText($id,'pdt_ender',
-							$this->GetProperty('ender'),20,50));
+						$mod->CreateInputText($id,'fp_privatename',
+							$this->GetProperty('privatename',$alias),20,50));
 		$main[] = array($mod->Lang('title_initial_count'),
-						$mod->CreateInputText($id,'pdt_repeatcount',
-							$this->GetProperty('repeatcount',1),2,2));
+						$mod->CreateInputText($id,'fp_repeatcount',
+							$this->GetProperty('repeatcount',1),2,2),
+						$mod->Lang('help_initial_count'));
+		$main[] = array($mod->Lang('title_min_count'),
+						$mod->CreateInputText($id,'fp_mincount',
+							$this->GetProperty('mincount',1),2,2),
+						$mod->Lang('help_min_count'));
 		$main[] = array($mod->Lang('title_max_count'),
-						$mod->CreateInputText($id,'pdt_maxcount',
-							$this->GetProperty('maxcount',0),2,2));
-
-		$this->RemoveAdminField($adv,$mod->Lang('title_field_javascript'));
-		$this->RemoveAdminField($adv,$mod->Lang('title_field_resources'));
+						$mod->CreateInputText($id,'fp_maxcount',
+							$this->GetProperty('maxcount',0),2,2),
+						$mod->Lang('help_limit_count'));
+		$main[] = array($mod->Lang('title_add_button_seq'),
+						$mod->CreateInputText($id,'fp_insert_label',
+							$this->GetProperty('insert_label',$mod->Lang('insert_sequence')),25,30));
+		$main[] = array($mod->Lang('title_del_button_seq'),
+						$mod->CreateInputText($id,'fp_delete_label',
+							$this->GetProperty('delete_label',$mod->Lang('delete_sequence')),25,30));
 
 		return array('main'=>$main,'adv'=>$adv);
 	}
 
-/*	public function PostAdminAction(&$params)
-	{
-		conform any enderfield::opt_starter
-	}
-
-	public function AdminValidate($id)
+/*	public function AdminValidate($id)
 	{
 		TODO
 		unique privatename
@@ -76,35 +143,18 @@ class SequenceStart extends FieldBase
 */
 	public function Populate($id,&$params)
 	{
-		/*
-		setup js:
-		$(BTNSHOW).css('display','none').click({
-			$(DIV).css('display','block');
-			$(this).css('display','none');
-			$(BTNHIDE).css('display','inline-block');
-		});
-		$(BTNHIDE).click({
-			$(DIV).css('display','none');
-			$(this).css('display','none');
-			$(BTNSHOW).css('display','inline-block');
-		});
-		if only 1 sequence
-			$(BTNREM).css('display','none');
-		*/
-		$name = $this->GetProperty('privatename'); //htmlentities?
-		$repeats = $this->GetProperty('repeatcount');
-		$ret = '';
-		for ($i=0; $i<$repeats; $i++) //OR support looping in form constructor
-		{
-			$ret = 'BTNHIDE BTNSHOW BTNADD BTNREM';
-			$ret .= '<br />'.htmlentities('>>>>').PHP_EOL;
-			$ret .= '<div id="'.$name.'" class="sequence"><!-- start sequence '.$name.' -->'.PHP_EOL;
-			//FIELD(S) STUFF
-			//?? HOWTO if NESTED ???
-			$ret .= '</div><!-- end sequence '.$name.' -->'.PHP_EOL;
-			$ret .= htmlentities('<<<<').PHP_EOL;
+		$html = '';
+		$bnm = $id.$this->formdata->current_prefix.$this->Id;
+		$bid = $this->GetInputId();
+//TODO no 'insert_label' sometimes, no 'delete_label' sometimes
+		foreach (array('insert_label','delete_label') as $key) {
+			$c = $TODO;
+			$tmp = '<input type="button" name="'.$bnm.$c.'" id="'.$bid.$c.
+			'" value="'.$this->GetProperty($key).'" />';
+			$html .= $this->SetClass($tmp).' ';
 		}
-		return $ret;
-		//TODO skip $formdata->Fields to relevant SequenceEnd
+		$html .= $this->Name;
+		//no js
+		return $html;
 	}
 }
