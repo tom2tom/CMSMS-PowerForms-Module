c13373e21623bcafbd8b24d1141c783f3eceda7c
tom2tom <tpgww@onepost.net>
2017-01-08 17:33:04 +1100

    bump copyright year

--- lib/class.Button.php
+++ lib/class.Button.php
@@ -1,6 +1,6 @@
 <?php
 # This file is part of CMS Made Simple module: PWForms
-# Copyright (C) 2012-2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2012-2017 Tom Phane <tpgww@onepost.net>
 # Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
 # Refer to licence and other details at the top of file PWForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
@@ -9,9 +9,9 @@ namespace PWForms;
 
 class Button extends FieldBase
 {
-	public function __construct(&$formdata,&$params)
+	public function __construct(&$formdata, &$params)
 	{
-		parent::__construct($formdata,$params);
+		parent::__construct($formdata, $params);
 		$this->ChangeRequirement = FALSE;
 		$this->DisplayInSubmission = FALSE;
 		$this->Type = 'Button';
@@ -19,15 +19,15 @@ class Button extends FieldBase
 
 	public function AdminPopulate($id)
 	{
-		list($main,$adv) = $this->AdminPopulateCommon($id,FALSE,TRUE);
+		list($main, $adv) = $this->AdminPopulateCommon($id, FALSE, TRUE);
 		$mod = $this->formdata->formsmodule;
 		$main[] = array($mod->Lang('title_button_text'),
-						$mod->CreateInputText($id,'fp_text',
-							$this->GetProperty('text'),40));
+						$mod->CreateInputText($id, 'fp_text',
+							$this->GetProperty('text'), 40));
 		return array('main'=>$main,'adv'=>$adv);
 	}
 
-	public function Populate($id,&$params)
+	public function Populate($id, &$params)
 	{
 		$tmp = '<input type="button" id="'.$this->GetInputId().'" name="'.
 			$id.$this->formdata->current_prefix.$this->Id.
