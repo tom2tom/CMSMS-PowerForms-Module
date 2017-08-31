a1df6fe9bd1d18182d833158d84be9085db0a3e8
tom2tom <tpgww@onepost.net>
2017-08-31 17:21:25 +1000

    code for non-ajax deletion

--- action.delete_field.php
+++ action.delete_field.php
@@ -1,13 +1,47 @@
 <?php
 # This file is part of CMS Made Simple module: PWForms
 # Copyright (C) 2012-2017 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
 # Refer to licence and other details at the top of file PWForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
 
 if (isset($params['active_tab'])) {
 	//we've reached here via a request or redirect
-	//TODO
+	try {
+		$cache = PWForms\Utils::GetCache($this);
+	} catch (Exception $e) {
+$TODO = $CRASH1;
+		echo 'system error';
+		return;
+	}
+	$formdata = $cache->get($params['datakey']);
+	if (is_null($formdata) || !$formdata->Fields) {
+$TODO = $CRASH2;
+		echo 'system error';
+		return;
+	}
+	if (is_array($params['field_id'])) {
+		$all = $params['field_id'];
+	} else {
+		$all = [$params['field_id']];
+	}
+	//null/mark field(s) for deletion during store
+	foreach ($all as $one) {
+		$fid = (int)$one;
+		$formdata->Fields[$fid] = NULL;
+		if ($formdata->FieldOrders) {
+			$key = array_search($fid, $formdata->FieldOrders);
+			if ($key !== FALSE) {
+				unset($formdata->FieldOrders[$key]);
+			}
+		}
+	}
+	$cache->set($params['datakey'], $formdata, 84600);
+
+	$this->Redirect($id, 'open_form', $returnid, [
+		'form_id'=>$params['form_id'],
+		'active_tab'=>$params['active_tab'],
+		'datakey'=>$params['datakey']
+	]);
 } else {
 	//we've reached here via an ajax-call
 	try {
@@ -21,7 +55,7 @@ if (isset($params['active_tab'])) {
 		echo '0';
 		exit;
 	}
-	//mark field(s) for deletion during Store
+	//null/mark field(s)
 	if (strpos($params['field_id'], ',') !== FALSE) {
 		$all = explode(',', $params['field_id']);
 	} else {
