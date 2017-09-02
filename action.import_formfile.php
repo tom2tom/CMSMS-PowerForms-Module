b75d5271b3d46cfe27c38d77594b49951d8f7677
tom2tom <tpgww@onepost.net>
2017-09-02 11:27:52 +1000

    supply new name, alias for import

--- action.import_formfile.php
+++ action.import_formfile.php
@@ -12,7 +12,7 @@ if (!$this->_CheckAccess('ModifyPFForms')) {
 $funcs = new PWForms\FormOperations();
 if ($funcs->NewID($params['import_formname'], $params['import_formalias'])) {
 	$fp = $_FILES[$id.'xmlfile']['tmp_name'];
-	$state = $funcs->ImportXML($this, $fp);
+	$state = $funcs->ImportXML($this, $fp, $params['import_formname'], $params['import_formalias']);
 	$key = ($state)?'form_imported':'err_form_import';
 } else {
 	$state = FALSE;
