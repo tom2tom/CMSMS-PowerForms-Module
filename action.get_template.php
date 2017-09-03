30036126e0ed20312d9f5b4b831810a0917c795a
tom2tom <tpgww@onepost.net>
2017-09-03 15:56:14 +1000

    add comment

--- action.get_template.php
+++ action.get_template.php
@@ -1,10 +1,9 @@
 <?php
 # This file is part of CMS Made Simple module: PWForms
 # Copyright (C) 2012-2017 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
 # Refer to licence and other details at the top of file PWForms.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerforms
-
+//get a recorded (main or submission) form-template or content of a template file
 //this action processes ajax-calls
 
 if (preg_match('/\.tpl$/', $params['tid'])) {
